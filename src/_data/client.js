c0684e9e2d081c849404eabc2ab7fac14d2d1def
Ivo_Sa92 <141323736+IvoSa92@users.noreply.github.com>
2024-11-04 19:08:41 +0100

    Client Data auf Platzhalter geändert

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+  name: "Name der Firma",
+  email: "Mail der Firma",
+  phoneForTel: "Telefonnumer",
+  address: {
+    lineOne: "First Address Line",
+    lineTwo: "Second Address Line",
+    city: "Denver",
+    state: "CO",
+    zip: "80206",
+    country: "US",
+    mapLink: "google maps link falls benötigt",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/",
+    instagram: "https://www.instagram.com/",
+  },
+
+  domain: "https://www.example.com",
 };
