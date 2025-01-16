cfeca5a373276ad6ec5ab7539e23a2024530aed0
Ivo_Sa92 <141323736+IvoSa92@users.noreply.github.com>
2025-01-16 17:58:05 +0100

    Enhance FAQ functionality with toggle and filter features
    - Added a toggle feature for FAQ items to improve user interaction.
    - Implemented a filter class to allow users to filter FAQ items based on categories.
    - Refactored event listener setup for better readability and maintainability.
    - Initialized the FAQ filter on page load for immediate user access.

--- src/assets/js/faq.js
+++ src/assets/js/faq.js
@@ -1,3 +1,4 @@
+// FAQ Items Toggle
 const faqItems = Array.from(document.querySelectorAll(".cs-faq-item"));
 for (const item of faqItems) {
   const onClick = () => {
@@ -6,44 +7,40 @@ for (const item of faqItems) {
   item.addEventListener("click", onClick);
 }
 
+// FAQ Filter
 class FAQFilter {
-  filtersSelector = ".cs-option";
-  FAQselector = ".cs-faq-group";
-  activeClass = "cs-active";
-  hiddenClass = "cs-hidden";
-
   constructor() {
-    const $filters = document.querySelectorAll(this.filtersSelector);
-    this.$activeFilter = $filters[0];
-    this.$images = document.querySelectorAll(this.FAQselector);
+    this.filtersSelector = ".cs-option";
+    this.FAQselector = ".cs-faq-group";
+    this.activeClass = "cs-active";
+    this.hiddenClass = "cs-hidden";
 
-    this.$activeFilter.classList.add(this.activeClass);
+    this.filters = document.querySelectorAll(this.filtersSelector);
+    this.activeFilter = this.filters[0];
+    this.images = document.querySelectorAll(this.FAQselector);
 
-    for (const $filter of $filters) {
-      $filter.addEventListener("click", () => this.onClick($filter));
+    this.activeFilter.classList.add(this.activeClass);
+
+    for (const filter of this.filters) {
+      filter.addEventListener("click", () => this.onClick(filter));
     }
   }
 
-  onClick($filter) {
-    this.filter($filter.dataset.filter);
-
-    const { activeClass } = this;
-
-    this.$activeFilter.classList.remove(activeClass);
-    $filter.classList.add(activeClass);
-
-    this.$activeFilter = $filter;
+  onClick(filter) {
+    this.filter(filter.dataset.filter);
+    this.activeFilter.classList.remove(this.activeClass);
+    filter.classList.add(this.activeClass);
+    this.activeFilter = filter;
   }
 
   filter(filter) {
     const showAll = filter == "all";
-    const { hiddenClass } = this;
-
-    for (const $image of this.$images) {
-      const show = showAll || $image.dataset.category == filter;
-      $image.classList.toggle(hiddenClass, !show);
+    for (const image of this.images) {
+      const show = showAll || image.dataset.category == filter;
+      image.classList.toggle(this.hiddenClass, !show);
     }
   }
 }
 
+// Initialize FAQ Filter
 new FAQFilter();
