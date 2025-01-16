2520f78c7115e5e8a6ec30c1fdd2d0f958c3b064
Ivo_Sa92 <141323736+IvoSa92@users.noreply.github.com>
2025-01-16 17:48:04 +0100

    Refactor font management by converting TTF to WOFF2 format and updating references
    - Implemented an asynchronous font conversion script to convert TTF files to WOFF2 format, improving font loading performance.
    - Updated CSS and SASS files to reference the new WOFF2 font files, ensuring consistency across the project.
    - Removed obsolete TTF font files to streamline asset management and reduce file size.
    - Added a script tag in base.html for improved functionality and compliance.

--- scripts/convert-fonts.js
+++ scripts/convert-fonts.js
@@ -1,17 +1,22 @@
 const fs = require("fs");
 const path = require("path");
-const ttf2woff2 = require("ttf2woff2");
 
 const fontsDir = path.join(__dirname, "../src/assets/fonts");
 
 // Konvertiere alle TTF Dateien
-fs.readdirSync(fontsDir)
-  .filter((file) => file.endsWith(".ttf"))
-  .forEach((file) => {
-    const input = fs.readFileSync(path.join(fontsDir, file));
-    const output = ttf2woff2(input);
-    const outputPath = path.join(fontsDir, file.replace(".ttf", ".woff2"));
+async function convertFonts() {
+  const ttf2woff2 = await import("ttf2woff2");
 
-    fs.writeFileSync(outputPath, output);
-    console.log(`Converted ${file} to ${file.replace(".ttf", ".woff2")}`);
-  });
+  fs.readdirSync(fontsDir)
+    .filter((file) => file.endsWith(".ttf"))
+    .forEach((file) => {
+      const input = fs.readFileSync(path.join(fontsDir, file));
+      const output = ttf2woff2.default(input);
+      const outputPath = path.join(fontsDir, file.replace(".ttf", ".woff2"));
+
+      fs.writeFileSync(outputPath, output);
+      console.log(`Converted ${file} to ${file.replace(".ttf", ".woff2")}`);
+    });
+}
+
+convertFonts().catch(console.error);
